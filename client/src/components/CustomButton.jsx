ec1001395f438472c838f111df6eaa8d3db62888
gracjaan <gchmielnicki3005@gmail.com>
2023-09-14 20:19:50 +0200

    Initial commit of button

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  )
+}
+
+export default CustomButton
