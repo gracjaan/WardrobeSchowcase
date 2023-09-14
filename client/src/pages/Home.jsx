31b5cbc8495b47f9d150e8a7adcdccd1d3d66d3b
gracjaan <gchmielnicki3005@gmail.com>
2023-09-14 20:10:55 +0200

    Adding animated header and nav

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import {
+  headContainerAnimation,
+  headTextAnimation,
+  headContentAnimation,
+  slideAnimation,
+} from "../config/motion";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const snap = useSnapshot(state);
 
-export default Home
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="threejs"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl: block hidden" /> DO IT. 
+              </h1>
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
